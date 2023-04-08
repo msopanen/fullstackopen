888e282608c3a2ace04f6890bee0b66a0e3527f1
msopanen <mikko.p.sopanen@gmail.com>
2023-04-08 07:55:12 -0400

    Add exercise 2.4 courseinfo step 9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,7 +2,7 @@ const Header = ({ name }) => <h1>{name}</h1>;
 
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, p) => (sum += p.exercises), 0);
-  return <p>Number of exercises {total}</p>;
+  return <b>total of {total} exercises</b>;
 };
 
 const Part = ({ part }) => (
@@ -11,13 +11,7 @@ const Part = ({ part }) => (
   </p>
 );
 
-const Content = ({ parts }) => (
-  <>
-    <Part part={parts[0]} />
-    <Part part={parts[1]} />
-    <Part part={parts[2]} />
-  </>
-);
+const Content = ({ parts }) => parts.map((part) => <Part part={part} />);
 
 const Course = ({ course }) => {
   return (
@@ -29,29 +23,52 @@ const Course = ({ course }) => {
   );
 };
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return courses.map((course) => <Course course={course} />);
 };
 
 export default App;
