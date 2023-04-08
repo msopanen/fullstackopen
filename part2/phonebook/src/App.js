f7321bb9d22f792d056aa9e474080c0fb9764763
msopanen <mikko.p.sopanen@gmail.com>
2023-04-08 12:48:18 -0400

    Add exercise 2.9 The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,17 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const isUniquePerson = ({ newPerson, persons }) => {
     return persons.every((p) => p.name !== newPerson.name);
   };
+
+  const nameIncludes =
+    (filter) =>
+    ({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase());
+
   const handleAddRecord = (event) => {
     event.preventDefault();
     const newPerson = { name: newName, number: newNumber };
@@ -28,9 +35,20 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleFilterInputChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons = persons.filter(nameIncludes(filter));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with{" "}
+        <input value={filter} onChange={handleFilterInputChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleAddRecord}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange} />
@@ -44,7 +62,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(({ name, number }) => (
+        {filteredPersons.map(({ name, number }) => (
           <div key={name}>
             {name} {number}
           </div>
