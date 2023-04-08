4371ee8dd6bec2328fb01f3cb444e1ebf86c5163
msopanen <mikko.p.sopanen@gmail.com>
2023-04-08 10:01:41 -0400

    Add exercise 2.6 The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,23 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleAddRecord = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    //setPersons(persons.concat([newPerson]));
+    setPersons([...persons, newPerson]);
+  };
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddRecord}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -18,7 +29,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map(({ name }) => (
-          <div>{name}</div>
+          <div key={name}>{name}</div>
         ))}
       </div>
     </div>
