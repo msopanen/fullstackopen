2f118bdcd99437e2e6e0d5694e7c5d89e25bdcb7
msopanen <mikko.p.sopanen@gmail.com>
2023-04-09 02:11:55 -0400

    Add exercise 2.11 The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Filter from "./Filter";
 import UserForm from "./UserForm";
 import Persons from "./Persons";
@@ -17,6 +18,12 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleAddUser = (event) => {
     event.preventDefault();
