596f772fd9958984e92289f8fb3175afbd655288
msopanen <mikko.p.sopanen@gmail.com>
2023-12-04 02:29:33 -0500

    Add exercise 3.8 The phonebook be step 8

--- part3/index.js
+++ part3/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 
+const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+// Create new morgan token and use morgan logger
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let records = [
   {
