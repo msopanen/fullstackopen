8aa44ee50e12dc4a32596e720b3611171c6777fc
msopanen <mikko.p.sopanen@gmail.com>
2023-12-07 03:30:09 -0500

    Add exercise 4.5 The Blog posts step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,9 +1,24 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogs) => {
+const totalLikes = (blogs = []) => {
   return blogs.reduce((sum, b) => sum += b.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs = []) => {
+  // eslint-disable-next-line no-unused-vars
+  const pick = ({ __v, _id, url, ...rest }) => rest
+  return blogs.reduce((f,b) => {
+    if(!f) {
+      f = pick(b)
+    }
+    else if(f.likes < b.likes) {
+      f = pick(b)
+    }
+    return f
+  }, null)
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
