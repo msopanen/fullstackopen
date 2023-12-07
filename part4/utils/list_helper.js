8c65b6acdfb2312b074ef27ac59ba170151b8c22
msopanen <mikko.p.sopanen@gmail.com>
2023-12-07 02:37:04 -0500

    Add exercise 4.4 The Blog posts step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,9 @@
-const dummy =(blogs) => {
+const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, b) => sum += b.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
