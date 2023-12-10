87ecfcaa56931fc866b4a1c6341fe17714560676
msopanen <mikko.p.sopanen@gmail.com>
2023-12-10 06:17:49 -0500

    Exercise 4.10 step3 async/await

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -11,14 +11,14 @@ blogsRouter.get("/", async (request, response, next) => {
   }
 })
 
-blogsRouter.post("/", (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    }).catch(err => next(err))
+blogsRouter.post("/", async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+    const result = await blog.save()
+    response.status(201).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 blogsRouter.delete("/:id", async (req, response, next) => {
