6a089edceb3a2c3c74ddc50473b294089d9d38cd
msopanen <mikko.p.sopanen@gmail.com>
2023-12-11 07:19:21 -0500

    Add exercise 4.18 The Blog posts step6

--- part4/app.js
+++ part4/app.js
@@ -2,14 +2,17 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 
+const loginRouter = require("./controllers/login")
 const usersRouter = require("./controllers/users")
 const blogsRouter = require("./controllers/blogs")
+
 const { requestLogger, unknownEndpoint, errorHandler } = require("./utils/middlewares")
 
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
+app.use("/api/login", loginRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/blogs", blogsRouter)
 
