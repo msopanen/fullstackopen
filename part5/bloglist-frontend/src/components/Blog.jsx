5ed971163e77fb2332f75032146f1fa41c67f827
msopanen <mikko.p.sopanen@gmail.com>
2023-12-15 03:49:38 -0500

    Add exercise 5.7 The Blog list frontend step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = (e) => {
+    e.preventDefault();
+    setShowDetails((prev) => !prev);
+  };
+
+  const blogStyle = {
+    border: "solid",
+    margin: "0.125rem",
+    padding: "0.125rem",
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={toggleDetails}>{showDetails ? "hide" : "show"}</button>
+      {showDetails && (
+        <div>
+          {blog.url}
+          <br />
+          {blog.likes} <button>like</button>
+          <br />
+          {blog.author}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
