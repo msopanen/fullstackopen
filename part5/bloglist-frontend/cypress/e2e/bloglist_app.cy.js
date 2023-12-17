f4b34df6a679bb2c3fd2160f4f707215737a2548
msopanen <mikko.p.sopanen@gmail.com>
2023-12-17 10:42:48 -0500

    Add exercise 5.18 Blog list e2e tests step2

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,4 +1,4 @@
-describe("when not logged in", function () {
+describe("blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
 
@@ -11,32 +11,34 @@ describe("when not logged in", function () {
     cy.visit("http://localhost:5173");
   });
 
-  it("login form is shown", function () {
-    cy.visit("http://localhost:5173");
-    cy.contains("log in to application");
-  });
-
-  describe("login", function () {
-    it("succeeds with correct credentials", function () {
-      cy.contains("log in to application").click();
-      cy.get("#username-input").type("ctester");
-      cy.get("#password-input").type("s3cr3t");
-      cy.get("#login-button").click();
-
-      cy.contains("Cypress Tester logged in");
+  describe("when not logged in", function () {
+    it("login form is shown", function () {
+      cy.visit("http://localhost:5173");
+      cy.contains("log in to application");
     });
 
-    it("fails with wrong credentials", function () {
-      cy.contains("log in to application").click();
-      cy.get("#username-input").type("ctester");
-      cy.get("#password-input").type("wrong-password");
-      cy.get("#login-button").click();
+    describe("login", function () {
+      it("succeeds with correct credentials", function () {
+        cy.contains("log in to application").click();
+        cy.get("#username-input").type("ctester");
+        cy.get("#password-input").type("s3cr3t");
+        cy.get("#login-button").click();
 
-      cy.get(".notification").should("contain", "wrong username or password");
-      cy.get(".notification").should("have.css", "color", "rgb(255, 0, 0)");
-      cy.get(".notification").should("have.css", "border-style", "solid");
+        cy.contains("Cypress Tester logged in");
+      });
 
-      cy.contains("log in to application");
+      it("fails with wrong credentials", function () {
+        cy.contains("log in to application").click();
+        cy.get("#username-input").type("ctester");
+        cy.get("#password-input").type("wrong-password");
+        cy.get("#login-button").click();
+
+        cy.get(".notification").should("contain", "wrong username or password");
+        cy.get(".notification").should("have.css", "color", "rgb(255, 0, 0)");
+        cy.get(".notification").should("have.css", "border-style", "solid");
+
+        cy.contains("Cypress Tester logged in").should("not.exist");
+      });
     });
   });
 });
