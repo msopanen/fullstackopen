0f0e0b93a294fe3c8fc849960ec5a42952175ce5
msopanen <mikko.p.sopanen@gmail.com>
2023-12-20 23:36:48 -0500

    Add exercise 6.21 query anecdotes

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import anecdoteService from "./services/anecdotes";
+import { getAnecdotes } from "./services/anecdotes";
 
 const App = () => {
   const handleVote = (anecdote) => {
@@ -11,13 +11,14 @@ const App = () => {
 
   const { isSuccess, isError, data } = useQuery({
     queryKey: ["anecdotes"],
-    queryFn: anecdoteService.getAnecdotes,
+    queryFn: getAnecdotes,
   });
 
   if (isError) {
     return <div>anecdote service not available due to problems in server</div>;
   }
 
+  console.log({ data });
   return (
     <div>
       <h3>Anecdote app</h3>
--- part6/query-anecdotes/src/services/anecdotes.js
+++ part6/query-anecdotes/src/services/anecdotes.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001";
+const baseUrl = "http://localhost:3001/anecdotes";
 
-const getAnecdotes = () =>
-  axios.get(`${baseUrl}/anecdotes`).then((res) => res.data);
+// get all anecdotes
+// -------------------------------------------------
+export const getAnecdotes = () =>
+  axios.get(`${baseUrl}`).then((res) => res.data);
 
-export default { getAnecdotes };
+// create new anecdote
+// -------------------------------------------------
+export const createAnecdote = (anecdote) => {
+  const object = asObject(anecdote);
+  return axios.post(baseUrl, object).then((res) => res.data);
+};
+
+const getId = () => (100000 * Math.random()).toFixed(0);
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0,
+  };
+};
