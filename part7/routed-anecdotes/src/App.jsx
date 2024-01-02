f640c4ceebc8dd4dcc7a9ebdaf40bfd31c06c6ee
msopanen <mikko.p.sopanen@gmail.com>
2024-01-02 07:55:41 -0500

    Add exercise 7.5 routed anecdotes and hooks step2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -152,6 +152,11 @@ const CreateNew = (props) => {
     navigate("/");
   };
 
+  const handleReset = (e) => {
+    e.preventDefault();
+    [content, author, info].forEach((f) => f.reset());
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -169,6 +174,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   );
